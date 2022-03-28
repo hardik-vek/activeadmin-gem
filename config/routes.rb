d438f6d52087907f722de4a9f2a2ed07e2a9c82c
hardik <hardikvekariya09@gmail.com>
2022-03-28 18:17:01 +0530

    active admin gem practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # root 'homes#index'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  resources :posts, only: [:show, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
